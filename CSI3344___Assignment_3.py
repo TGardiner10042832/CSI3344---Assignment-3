9d39c8e78c06ab6d02a7755fb77a1c472d6db33f
Tristan Gardiner <tristang@our.ecu.edu.au>
2024-05-04 11:33:45 +0800

    Add project files.

---
+++ CSI3344___Assignment_3.py
@@ -0,0 +1,226 @@
+
+"""
+CSI3344 - Assignment 3
+Interface Component
+Group 33
+Tristan Gardiner
+Kayl Grau
+"""
+
+
+## Works out the average of the unit scores.
+## Takes a list and works out the average.
+def Average(results):
+    
+    # Create a list of the values.
+    #marks = results
+
+    # Init
+    total = 0
+    gradeCounter = 0
+
+    n = len(results)
+    i = 0
+
+    if (n == 0):
+        return 0
+
+    while results[i] != -1:
+        gradeValue = results[i]
+        total = total + gradeValue
+        gradeCounter += 1
+        i += 1
+
+    if (gradeCounter != 0):
+        average = total/gradeCounter
+        return average
+    else:
+        return 0
+
+"""
+    Manual entry of the student unit and scores.
+    Asks the user to enter their unit and results and returns list of Unit Codes and Scores.
+    Must allow multiples of unit codes and scores.
+    Checks if scores are correct at the end. 
+"""
+def StudentScores():
+
+    unitCode = []
+    unitScore = []
+
+    while True:
+        # Get correct input for Unit code and unit score.
+        x = UCEntry()
+        y = USEntry()
+
+        if x and y:
+            if x != -1 and y != -1:
+                unitCode.append(x)
+                unitScore.append(y)
+                #scores[x] = y
+            else:
+                unitCode.append(x)
+                unitScore.append(y)
+                #scores[x] = y
+                break
+
+    # Print the code for review
+    for i in range(len(unitCode) - 1):
+        print("Unit Code: ", unitCode[i], "\tUnit Score: ", unitScore[i]) 
+
+    ## If scores are not correct, re-enter values, or quit
+    while True:
+        correct = input("\nIs this correct? (y or n, or press q to Quit)")
+        if correct == "y":
+            return unitCode, unitScore
+            break
+        elif correct == "n":
+            StudentScores()
+            break
+        elif correct == "q":
+            break
+        else:
+            print("Please enter y or n")
+
+
+"""
+    Entry of correct Unit code
+    A unit code needs to be a string with the length of 7 characters and numbers.
+    Unless it is -1, to signify that the list is complete.
+"""
+def UCEntry():
+    
+    # Loop until the desired input is acheived.
+    while True:
+        unitCode = input("\nPlease enter unit code (enter -1 to finish): ")
+
+        if len(unitCode) == 7 or unitCode == "-1":
+            return unitCode
+            break
+        else:
+            print("Please enter a valid, 7 character unit code. Or -1 to finish the list.")
+
+
+"""
+    Entry of a Unit score
+    A unit score must be a valid number between 0-100, as a percentage.
+    It must be entered without an exception when converted to a double.
+    To finalise the list, -1 must be entered. 
+"""
+def USEntry():
+    
+    ## Loop until the desired input is achieved.
+    while True:
+
+        unitScore = input("Please enter unit results (enter -1 to finish): ")
+
+        try:
+            if int(unitScore) < 101 and int(unitScore) >= -1:
+                return int(unitScore)
+                break
+            else:
+                print("Please enter a valid score as a percentage (between 0 and 100).")
+        except:
+            print("Please enter as a valid number")
+
+
+
+
+## Yes or No - answers the question
+## Just a simple y or n loop, return value y or n
+def yesOrNo():
+
+    while True:
+        answer = input("Please enter Yes(y) or No(n) (Enter q to Quit)")
+        if answer == "y":
+            return answer
+            break
+        elif answer == "n":
+            return answer
+            break
+        elif answer == "q":
+            return answer
+            break
+        else:
+            print("Please enter y or n")
+
+
+## Student information input
+def studentInformation():
+
+    while True:
+        studentID = input("Please enter your 8-digit student ID number.")
+        fName = input("Please enter your first name: ")
+        lName = input("Please enter your last name: ")
+        email = input("Please enter your student email: ")
+
+        print("First name: ", fName, "Last Name: ", "Email: ", email)
+
+        print("Is this information correct?")
+        ans = yesOrNo()
+
+        if ans == "y":
+            studentInfo = [studentID, fName, lName, email]
+            return studentInfo
+        elif ans == "n":
+            studentInformation()
+        else:
+            break
+
+
+###     Menu's      ###
+
+# login / start menu
+def login():
+    
+    print("Welcome to the HEPaS system for OUST students\n\n")
+    print("Are you an existing student? (Former or Current)")
+    ans = yesOrNo()
+
+    if ans == "y":
+        print("\nPlease enter the following information.")
+        studentInfo = studentInformation()
+
+        ## This is were we verify user information with the server 
+        ## and then return the unit results on the system to display on screen.
+
+    elif ans == "n":
+        # Not a student, enter the information as well as their completed units and their results.
+        print("\nPlease enter the following information.")
+        studentInfo = studentInformation()
+        
+        ## for manual entry of unit codes and scores.
+        studentScores = StudentScores()
+        print(studentScores)
+
+        ## average scores (Takes an a dictionary as a parameter)
+        averageScore = Average(studentScores[1])
+        print("Your average mark is ", averageScore)
+
+        ## This is were we verify user information with the server 
+        ## and then return the unit results on the system to display on screen.
+
+
+        
+
+### Main directory
+""" 
+Things to do:
+    - login / login menu
+    - 
+
+"""
+def main():
+
+    ## Start menu (login)
+    login()
+
+
+    
+
+
+
+if __name__ == "__main__":
+    main()
+
+
